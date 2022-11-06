40881ad80cbaae1286626664d35513e7d7c05a6f
mbidadfar <mbidadfar@gmail.com>
2022-11-06 01:59:00 -0800

    edit4

--- js/inner.js
+++ js/inner.js
@@ -10,27 +10,6 @@ function showSide() {
 
 }
 
-// modal1
-var modal = document.getElementById("myModal");
-
-var btn = document.getElementById("myBtn");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
-// end modal1
 
 // modal2
 var modal2 = document.getElementById("myModal2");
