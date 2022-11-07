245a0d27355f7fdd108746195c9311d020f182ba
mbidadfar <mbidadfar@gmail.com>
2022-11-07 03:39:04 -0800

    modal

--- js/inner.js
+++ js/inner.js
@@ -9,3 +9,50 @@ function showSide() {
     }
 
 }
+
+// modal1
+var modal1 = document.getElementById("myModal1");
+
+var btn1 = document.getElementById("modal-1");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target1 == modal1) {
+    modal1.style.display = "none";
+  }
+} 
+
+// end modal1
+
+// modal2
+var modal2 = document.getElementById("myModal2");
+
+var btn2 = document.getElementById("modal-2");
+
+var span2 = document.getElementsByClassName("close2")[0];
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+} 
+
+// end modal2
